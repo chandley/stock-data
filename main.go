c13e962198ba5d0d33374ca366c0330a2d642de5
Chris Handley <chrisrhandley@gmail.com>
2016-12-01 10:50:41 +0000

    get and display time series data from quandl

--- main.go
+++ main.go
@@ -4,6 +4,7 @@ import (
 	"net/http"
 	"fmt"
 	"os"
+	"io/ioutil"
 )
 
 func handler(w http.ResponseWriter, r *http.Request) {
@@ -12,7 +13,24 @@ func handler(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "stock price of Ford motor is " + fordPrice)
 }
 
+func keyHandler(w http.ResponseWriter, r *http.Request) {
+	var apiKey string = os.Getenv("QUANDL_API_KEY")
+	fmt.Fprintf(w, "api key is " + apiKey)
+}
+
+func rawHandler(w http.ResponseWriter, r *http.Request) {
+	var apiKey string = os.Getenv("QUANDL_API_KEY")
+	resp, _ := http.Get("https://www.quandl.com/api/v3/datasets/WIKI/F.json?api_key=" + apiKey)
+	defer resp.Body.Close()
+	htmlData, _ := ioutil.ReadAll(resp.Body)
+
+	fmt.Fprintf(w, string(htmlData))
+
+}
+
 func main() {
 	http.HandleFunc("/", handler)
+	http.HandleFunc("/key", keyHandler)
+	http.HandleFunc("/raw", rawHandler)
 	http.ListenAndServe(":8080",nil)
 }
