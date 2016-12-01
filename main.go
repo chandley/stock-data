67f782277ffef829b583811d4ccf88c33f870e20
Chris Handley <chrisrhandley@gmail.com>
2016-12-01 14:53:56 +0000

    refactor for clarity

--- main.go
+++ main.go
@@ -12,7 +12,6 @@ import (
 
 type TimeSeries struct {
 	Dataset struct {
-			ID int `json:"id"`
 			DatasetCode string `json:"dataset_code"`
 			DatabaseCode string `json:"database_code"`
 			Name string `json:"name"`
@@ -23,22 +22,54 @@ type TimeSeries struct {
 			ColumnNames         []string `json:"column_names"`
 			Frequency           string `json:"frequency"`
 			Type                string `json:"type"`
-			Premium             bool `json:"premium"`
-			Limit               interface{} `json:"limit"`
-			Transform           interface{} `json:"transform"`
 			ColumnIndex         int `json:"column_index"`
 			StartDate           string `json:"start_date"`
 			EndDate             string `json:"end_date"`
 			PriceSeries         []PriceDayData `json:"data"`
-			Collapse            interface{} `json:"collapse"`
-			Order               interface{} `json:"order"`
-			DatabaseID          int `json:"database_id"`
 		} `json:"dataset"`
 }
 
 type PriceDayData struct{
 	Date string
 	ClosingPrice float64
+}
+
+func main() {
+	http.HandleFunc("/", jsonHandler)
+	http.ListenAndServe(":8080",nil)
+}
+
+func jsonHandler(w http.ResponseWriter, r *http.Request) {
+	var url string = generateUrl("FB")
+	fetchedTimeSeries := new(TimeSeries)
+	getJson(url, fetchedTimeSeries)
+
+	fmt.Fprintf(w, "<h1>%v</h1>", fetchedTimeSeries.Dataset.Name)
+
+	lastDay := fetchedTimeSeries.Dataset.PriceSeries[0]
+	fmt.Fprintf(w, "<p>Close price on %v was <b>%.2f</b></p>", lastDay.Date, lastDay.ClosingPrice)
+
+	fmt.Fprintf(w, "<h3>%v price graph</h3>", fetchedTimeSeries.Dataset.ColumnNames[1])
+
+	fmt.Fprintf(w, "<body>")
+	fmt.Fprint(w, generateChart(fetchedTimeSeries))
+	fmt.Fprintf(w, "</body>")
+}
+
+func generateUrl(ticker string) string {
+	var justDateAndClose = "column_index=4&"
+	var apiFilter string = justDateAndClose
+	var apiKey string = os.Getenv("QUANDL_API_KEY")
+	return "https://www.quandl.com/api/v3/datasets/WIKI/" + ticker +".json?" + apiFilter + "api_key=" + apiKey
+}
+
+func getJson(url string, target interface{}) error {
+	r, err := http.Get(url)
+	if err != nil {
+		return err
+	}
+	defer r.Body.Close()
+	return json.NewDecoder(r.Body).Decode(target)
 }
 
 func (n *PriceDayData) UnmarshalJSON(buf []byte) error {
@@ -53,48 +84,17 @@ func (n *PriceDayData) UnmarshalJSON(buf []byte) error {
 	return nil
 }
 
-
-func getJson(url string, target interface{}) error {
-	r, err := http.Get(url)
-	if err != nil {
-		return err
-	}
-	defer r.Body.Close()
-
-	return json.NewDecoder(r.Body).Decode(target)
-}
-
-func generateUrl(ticker string) string {
-	var justDateAndClose = "column_index=4&"
-	var apiFilter string = justDateAndClose + "start_date=2016-11-20&"
-	var apiKey string = os.Getenv("QUANDL_API_KEY")
-	return "https://www.quandl.com/api/v3/datasets/WIKI/" + ticker +".json?" + apiFilter + "api_key=" + apiKey
-}
-
 func getXYvals(ts *TimeSeries)  (xValues []time.Time, yValues []float64,) {
 	for _, dayData := range ts.Dataset.PriceSeries {
 		xDate, _ := time.Parse("2006-01-02", dayData.Date)
-		fmt.Println(time.Parse("2006-01-02", dayData.Date))
 		xValues = append(xValues, xDate)
 		yValues = append(yValues, dayData.ClosingPrice)
 	}
 	return
 }
 
-func jsonHandler(w http.ResponseWriter, r *http.Request) {
-	var url string = generateUrl("F")
-	fordSeries := new(TimeSeries)
-	getJson(url, fordSeries)
-	fmt.Fprintf(w, "<p>")
-	fmt.Fprintf(w, fordSeries.Dataset.Name)
-	fmt.Fprintf(w, "\n")
-	xFord, yFord := getXYvals(fordSeries)
-	fmt.Fprintf(w, "\n")
-	fmt.Println(yFord)
-	lastDay := fordSeries.Dataset.PriceSeries[0]
-	fmt.Fprintf(w, "Close price on %v was %.2f </p>", lastDay.Date, lastDay.ClosingPrice)
-	fmt.Fprintf(w, "<body>")
-
+func generateChart(ts *TimeSeries) *bytes.Buffer {
+	xSeries, ySeries := getXYvals(ts)
 	graph := chart.Chart{
 		XAxis: chart.XAxis{
 			Style: chart.Style{
@@ -108,19 +108,14 @@ func jsonHandler(w http.ResponseWriter, r *http.Request) {
 		},
 		Series: []chart.Series{
 			chart.TimeSeries{
-				XValues: xFord,
-				YValues: yFord,
+				XValues: xSeries,
+				YValues: ySeries,
 			},
 		},
 	}
 
 	buffer := bytes.NewBuffer([]byte{})
 	_ = graph.Render(chart.SVG, buffer)
-	fmt.Fprint(w, buffer)
-	fmt.Fprintf(w, "</body>")
+	return buffer
 }
 
-func main() {
-	http.HandleFunc("/", jsonHandler)
-	http.ListenAndServe(":8080",nil)
-}
