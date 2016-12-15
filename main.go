0279feffe79bfc414d3ff32dedaf5c3bd11008bc
Chris Handley <chrisrhandley@gmail.com>
2016-12-15 15:40:32 +0000

    wip

--- main.go
+++ main.go
@@ -32,7 +32,7 @@ func handler(w http.ResponseWriter, r *http.Request) {
 
 func fundamentalsHandler(w http.ResponseWriter, r *http.Request) {
 	_, data := getFundamentals.QuarterlyData()
-	fmt.Fprintf(w, "<p>Got data %v</p>", data)
+	fmt.Fprintf(w, "<p>Got data %v</p>", data.Result.Rows[0].Values[1].Value)
 }
 
 func showErrorPage(w http.ResponseWriter, ticker string, err error) {
